dbd0e6fdfa6631b9228fa96c843e7ea1bf1b0b3b
yondergarden <154359800+yondergarden@users.noreply.github.com>
2023-12-25 13:14:03 -0700

    Formatting mobile a bit

--- src/pages/home.js
+++ src/pages/home.js
@@ -101,7 +101,8 @@ const Home = () => {
 
   return (
       <header>
-        <body >
+        <body>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"/>
         <DisplayBackground />
         {aboutOpen ? <DisplayInfo/> : <DisplayMenu />}
         </body>
