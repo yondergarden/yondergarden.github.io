0be185fdc5ff84469cd1944e73fe9c28718d22ad
yondergarden <154359800+yondergarden@users.noreply.github.com>
2024-02-18 15:12:12 -0700

    Episode background fixed

--- src/components/Background/EpisodeBackground.js
+++ src/components/Background/EpisodeBackground.js
@@ -7,37 +7,25 @@ const Background = (props) => {
     let yonderFrameImage = "https://cdn.glitch.global/4a3253fe-3104-44d3-8a31-8f43e023369d/yonderGardenFrameFg.png?v=1702515498646"
     let titleImage = "https://i.imgur.com/11iaWFH.png"
 
-    // let backgroundImage = "https://cdn.glitch.global/4a3253fe-3104-44d3-8a31-8f43e023369d/circles.gif?v=1702433383009"
-    let backgroundImage = "https://cdn.glitch.me/7992739c-ee27-4d90-bcf4-fa82c0c89316/bgCircles.mp4?v=1708196568924"
+    let backgroundImage = "https://cdn.glitch.global/4a3253fe-3104-44d3-8a31-8f43e023369d/circles.gif?v=1702433383009"
+    // let backgroundImage = "https://cdn.glitch.me/7992739c-ee27-4d90-bcf4-fa82c0c89316/bgCircles.mp4?v=1708289495070"
     let yonderGrassImage = "https://cdn.glitch.global/4a3253fe-3104-44d3-8a31-8f43e023369d/grassLandscape.png?v=1702081026940"
 
     const DisplayBackground = () => {
         console.log(props)
         return (
 
-          <div>
 
+
+          <div>
             <div className={props.premiumOpen ? 'bind-bg-premium' : 'bind-bg'}>
-              <video className="bg-align" autoPlay muted loop>
-                  <source src={backgroundImage} type="video/mp4"/>
-                  Your browser does not support the video tag.
-              </video>
-              <video className="bg-align" autoPlay muted loop>
-                  <source src={backgroundImage} type="video/mp4"/>
-                  Your browser does not support the video tag.
-              </video>
-              <video className="bg-align" autoPlay muted loop>
-                  <source src={backgroundImage} type="video/mp4"/>
-                  Your browser does not support the video tag.
-              </video>
-              
-              // <img className="bg-align" src={backgroundImage} alt="" />
-              // <img className="bg-align" src={backgroundImage} alt="" />
-              // <img className="bg-align" src={backgroundImage} alt="" />
-              // <img className="bg-align" src={backgroundImage} alt="" />
-              // <img className="bg-align" src={backgroundImage} alt="" />
-              // <img className="bg-align" src={backgroundImage} alt="" />
-              // <img className="bg-align" src={backgroundImage} alt="" />
+              <img className="bg-align" src={backgroundImage} alt="" />
+              <img className="bg-align" src={backgroundImage} alt="" />
+              <img className="bg-align" src={backgroundImage} alt="" />
+              <img className="bg-align" src={backgroundImage} alt="" />
+              <img className="bg-align" src={backgroundImage} alt="" />
+              <img className="bg-align" src={backgroundImage} alt="" />
+              <img className="bg-align" src={backgroundImage} alt="" />
             </div>
             <div className={props.premiumOpen ? 'bind-bg-premium' : 'bind-bg'}>
               <img className="bg-align" src={yonderGrassImage} alt="" />
@@ -55,10 +43,10 @@ const Background = (props) => {
 
 
     return (
-        <div>
-            <DisplayBackground/>
-        </div>
-        );
+      <div>
+          <DisplayBackground/>
+      </div>
+    );
 }
 
 export default Background;
