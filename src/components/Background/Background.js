5f7fbc26f244650af8114129f892850656e1df4d
yondergarden <154359800+yondergarden@users.noreply.github.com>
2024-07-01 18:26:11 -0600

    background fix

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -3,12 +3,10 @@ import './Background.css';
 
 
 const Background = () => {
-    let yonderFrameImage = "https://i.imgur.com/9imLhpu.png"
-    let titleImage = "https://i.imgur.com/11iaWFH.png"
-
-    let backgroundImage = "https://homescreenbg.s3.us-east-2.amazonaws.com/homeScreenBg.mp4"
-    let backgroundImageMobile = "https://homescreenbg.s3.us-east-2.amazonaws.com/homeScreenBgMobile.mp4";
-    let yonderGrassImage = "https://cdn.glitch.global/4a3253fe-3104-44d3-8a31-8f43e023369d/grassLandscape.png?v=1702081026940"
+    let titleImage = "https://yondergarden.s3.us-east-2.amazonaws.com/defaultassets/TitleDefault.png"
+    let backgroundImage = "https://yondergarden.s3.us-east-2.amazonaws.com/defaultassets/BackgroundCirclesDefault.mp4"
+    let frameImage = "https://yondergarden.s3.us-east-2.amazonaws.com/defaultassets/FrameDefault.png";
+    let yonderGrassImage = "https://yondergarden.s3.us-east-2.amazonaws.com/defaultassets/GrassDefault.png"
 
     const [isPortrait, setIsPortrait] = useState(window.innerHeight > window.innerWidth);
 
@@ -26,16 +24,26 @@ const Background = () => {
 
     const DisplayBackground = () => {
         return (
-          <div>
-              <video className={`home-bg-video ${isPortrait ? 'hidden' : ''}`} autoPlay muted loop playsInline>
-                  <source src={backgroundImage} type="video/mp4"/>
-                  Your browser does not support the video tag.
-              </video>
-              <video className={`home-bg-video ${isPortrait ? '' : 'hidden'}`} autoPlay muted loop playsInline>
-                  <source src={backgroundImageMobile} type="video/mp4"/>
-                  Your browser does not support the video tag.
-              </video>
+          <div style={{ position: 'relative', height: '100vh' }}>
+              {[...Array(5)].map((_, i) => (
+                  <video
+                      key={i}
+                      className='home-bg-video'
+                      autoPlay
+                      muted
+                      loop
+                      playsInline
+                      style={{ zIndex: -i, position: 'absolute', top: '50%', transform: `translate(-50%, ${-50 + i * -10}vh)`}} // Adjust 50px to change the offset
+                  >
+                      <source src={backgroundImage} type="video/mp4"/>
+                      Your browser does not support the video tag.
+                  </video>
+              ))}
+              <img className='home-frame' src={yonderGrassImage}/>
+              <img className='home-frame' src={frameImage}/>
+              <img className='home-frame' src={titleImage}/>
           </div>
+
         );
       }
 
